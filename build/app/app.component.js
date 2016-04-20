fd136e4355f88a098bd91325a3532575f086b642
Shayne Boyer <shayne.p.boyer@disney.com>
2016-04-20 11:48:06 -0400

    change title

--- build/app/app.component.js
+++ build/app/app.component.js
@@ -24,7 +24,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>My First Angular 2 App</h1>'
+                        template: '<h1>Hello Kevin</h1>'
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
